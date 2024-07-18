caaca1cdebf387dd5df865cf45757f77e43821bb
ruanlimah <ruanlimah@hotmail.com>
2024-07-18 12:15:45 -0300

    primeiro commit

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,11 @@
+import styles from './Rodape.module.css';
+
+function Rodape() {
+    return (
+        <footer className={styles.rodape}>
+            <h2>Desenvolvido por Ruan Lima.</h2>
+        </footer>
+    )
+}
+
+export default Rodape;
